e8f5426acd7f35830a7037d698101a503b649a76
ludson96 <ludson_ps27@hotmail.com>
2025-09-10 09:59:47 -0300

    feat: add info employees

--- src/main/java/com/projedata/ProjedataApplication.java
+++ src/main/java/com/projedata/ProjedataApplication.java
@@ -18,14 +18,14 @@ public class ProjedataApplication  {
     // 3.1 – Inserir funcionáriosa
     employees.add(new Employee("Maria", LocalDate.of(2000, 10, 18), new BigDecimal("2009.44"), "Operador"));
     employees.add(new Employee("João", LocalDate.of(1990, 5, 12), new BigDecimal("2284.38"), "Operador"));
-    employees.add(new Employee("Caio", LocalDate.of(, , ), new BigDecimal(""), ""));
-    employees.add(new Employee("Miguel", LocalDate.of(, , ), new BigDecimal(""), ""));
-    employees.add(new Employee("Alice", LocalDate.of(, , ), new BigDecimal(""), ""));
-    employees.add(new Employee("Heitor", LocalDate.of(, , ), new BigDecimal(""), ""));
-    employees.add(new Employee("Arthur", LocalDate.of(, , ), new BigDecimal(""), ""));
-    employees.add(new Employee("Laura", LocalDate.of(, , ), new BigDecimal(""), ""));
-    employees.add(new Employee("Heloísa", LocalDate.of(, , ), new BigDecimal(""), ""));
-    employees.add(new Employee("Helena", LocalDate.of(, , ), new BigDecimal(""), ""));
+    employees.add(new Employee("Caio", LocalDate.of(1961, 5, 12), new BigDecimal("9836.14"), "Coordenador"));
+    employees.add(new Employee("Miguel", LocalDate.of(1988, 10, 14), new BigDecimal("19119.88"), "Diretor"));
+    employees.add(new Employee("Alice", LocalDate.of(1995, 1, 5), new BigDecimal("2234.68"), "Recepcionista"));
+    employees.add(new Employee("Heitor", LocalDate.of(1999, 11, 19), new BigDecimal("1582.72"), "Operador"));
+    employees.add(new Employee("Arthur", LocalDate.of(1993, 3, 31), new BigDecimal("4071.84"), "Contador"));
+    employees.add(new Employee("Laura", LocalDate.of(1994, 7, 8), new BigDecimal("3017.45"), "Gerente"));
+    employees.add(new Employee("Heloísa", LocalDate.of(2003, 5, 24), new BigDecimal("1606.85"), "Eletricista"));
+    employees.add(new Employee("Helena", LocalDate.of(1996, 9, 2), new BigDecimal("2799.93"), "Gerente"));
 // teste
     // 3.2 – Remover funcionário "João"
     employees.removeIf(f -> f.getNome().equalsIgnoreCase("João"));
