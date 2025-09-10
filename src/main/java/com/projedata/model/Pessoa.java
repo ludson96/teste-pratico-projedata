9ddbd8e39fe61f4873c7a76a10bdf83603f7a707
ludson96 <ludson_ps27@hotmail.com>
2025-09-10 18:30:39 -0300

    docs: add javadoc for Pessoa.java

--- src/main/java/com/projedata/model/Pessoa.java
+++ src/main/java/com/projedata/model/Pessoa.java
@@ -2,28 +2,65 @@ package main.java.com.projedata.model;
 
 import java.time.LocalDate;
 
-// 1– Classe Pessoa com os atributos: nome (String) e data nascimento (LocalDate).
+/**
+ * Represents a person with a name and date of birth.
+ */
 public class Pessoa {
+
+  /**
+   * The person's name.
+   */
+
   private String nome;
+  /**
+   * The person's date of birth.
+   */
+
   private LocalDate dataNascimento;
 
+  /**
+   * Constructs a new Pessoa object.
+   *
+   * @param nome           The person's name.
+   * @param dataNascimento The person's date of birth.
+   */
   public Pessoa(String nome, LocalDate dataNascimento) {
     this.nome = nome;
     this.dataNascimento = dataNascimento;
   }
 
+  /**
+   * Gets the person's name.
+   *
+   * @return The name as a String.
+   */
   public String getNome() {
     return nome;
   }
 
+  /**
+   * Sets the person's name.
+   *
+   * @param nome The new name to set.
+   */
   public void setNome(String nome) {
     this.nome = nome;
   }
 
+  /**
+   * Gets the person's date of birth.
+   *
+   * @return The date of birth as a LocalDate.
+   */
   public LocalDate getDataNascimento() {
     return dataNascimento;
   }
 
+  /**
+   * Sets the person's date of birth.
+   *
+   * @param dataNascimento The new date of birth to set.
+   */
   public void setDataNascimento(LocalDate dataNascimento) {
     this.dataNascimento = dataNascimento;
   }
